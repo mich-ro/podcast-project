62bdafb9e875e7e7ac6c9946912c695813e43350
michaela <michaelaro94@gmail.com>
2022-11-22 19:48:05 -0500

    auth0 configured

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-uin022d5nc7lyh5l.us.auth0.com"
+    clientId="IvFbWORAIyDnK9fFCrkO7eVlc12xrYeX"
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 )
